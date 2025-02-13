617b1df742c79eef5f345e15260221b20c5091ae
Parkdev <43129272+Parkdev@users.noreply.github.com>
2025-02-13 15:33:46 +0900

    before upload azure

--- function_app.py
+++ function_app.py
@@ -3,30 +3,37 @@ from dto.question import QuestionRequest
 
 from azure.messaging.webpubsubservice.aio import WebPubSubServiceClient
 from azure.core.credentials import AzureKeyCredential
+from fastapi.middleware.cors import CORSMiddleware
 
 import azure.functions as func
 import os
+import uuid
 
 fast_app = FastAPI()
+#CORS 문제 해결
+fast_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app = func.AsgiFunctionApp(app=fast_app , http_auth_level=func.AuthLevel.ANONYMOUS )
 pubsub_client = WebPubSubServiceClient(endpoint=os.environ['PUBSUB_CONNECTION_URL'], 
                                        hub=os.environ['PUBSUB_HUB'], 
                                        credential=AzureKeyCredential(os.environ['PUBSUB_KEY']))
 
-# 만들어야할 코드
-# 1. 질문 api
-# 2. Pubsub 토큰 발급 API
+# 채널 id 가져오기
+@fast_app.get("/channel-id")
+async def get_channel_id():
+    return {"channel_id": str(uuid.uuid4())}
+    # TODO: DB에 중복된 값이 있으면 재생성
 
-# 1. 질문 api
+# 질문 전송
 @fast_app.post("/question")
 async def send_qusetion(request: QuestionRequest):
     return request
 
+# 토큰 발급
 @fast_app.get("/pubsub/token")
 async def read_root(channel_id: str):
     return await pubsub_client.get_client_access_token(groups=[channel_id], minutes_to_expire=5, role=['webpubsub.joinLeaveGroup.' + channel_id])
-
-
-# @app.get("/items/{item_id}")
-# async def read_item(item_id: int, q: Optional[str] = None):
-#     return {"item_id": item_id, "q": q}
